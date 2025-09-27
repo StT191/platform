586e14c4fafd5abc78065bc0fbbd00fb6a530564
Stefan Tammer <st.tammer@gmail.com>
2025-09-27 16:20:40 +0200

    improve local storage api

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -16,7 +16,7 @@ pub use web::*;
 
 // facade type with implicit error-handling
 
-use anyhow::{Result as Res, Error, format_err};
+use anyhow::{Result as Res, Error, anyhow};
 
 pub struct LocalStorageFacade {
     inner: Res<LocalStorage>,
@@ -27,16 +27,21 @@ impl LocalStorageFacade {
 
     pub fn inner(&self) -> &Res<LocalStorage> { &self.inner }
 
-    pub fn new(qualifier: &str, organization: &str, application: &str, error_handler: impl Fn(&Error) + 'static) -> Self {
-        let inner = LocalStorage::new(qualifier, organization, application).inspect_err(&error_handler);
+    pub fn from_inner(inner: Res<LocalStorage>, error_handler: impl Fn(&Error) + 'static) -> Self {
         Self { inner, error_handler: Box::new(error_handler) }
     }
 
+    pub fn from_storage(inner: LocalStorage, error_handler: impl Fn(&Error) + 'static) -> Self {
+        Self::from_inner(Ok(inner), error_handler)
+    }
+
+    pub fn new(qualifier: &str, organization: &str, application: &str, error_handler: impl Fn(&Error) + 'static) -> Self {
+        let inner = LocalStorage::new(qualifier, organization, application).inspect_err(&error_handler);
+        Self::from_inner(inner, error_handler)
+    }
+
     pub fn empty() -> Self {
-        Self {
-            inner: Err(format_err!("Empty LocalStorageFacade")),
-            error_handler: Box::new(|_err| {}),
-        }
+        Self::from_inner(Err(anyhow!("Empty LocalStorageFacade")), |_err| {})
     }
 
     pub fn set(&self, key: &str, value: &str) {
